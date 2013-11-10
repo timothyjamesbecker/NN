fed4602196d9ca3a475d2d64537d9d94e04bf1bf
timothyjamesbecker <tbecker@10.0.0.6>
2013-11-10 14:53:10 -0500

    sb data structure class

---
+++ sb.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   sb.h
+ * Author: tbecker
+ *
+ * Created on November 4, 2013, 8:47 PM
+ */
+
+#ifndef SB_H
+#define	SB_H
+
+class sb {
+public:
+    sb();
+    sb(const sb& orig);
+    virtual ~sb();
+private:
+
+};
+
+#endif	/* SB_H */
+
